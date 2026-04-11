1b030f2a639275044e01a310ad0ccba04a58fa8e
Damian Marchewka <enki6@o2.pl>
2026-04-11 23:54:01 +0200

    Fully CRUD tests implementation

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -11,3 +11,48 @@ def test_create_task(client: TestClient):
     assert response.status_code == 201
     assert data["title"] == "Test title"
     assert data["id"] is not None
+
+
+def test_get_task(client: TestClient):
+    response = client.post("/tasks/", json={
+        "title": "Test title",
+        "description": "Test description"
+    })
+    data = response.json()
+    task_id = data["id"]
+    result = client.get(f"/tasks/{task_id}")
+    result_data = result.json()
+    assert result.status_code == 200
+    assert result_data["id"] == task_id
+    assert result_data["title"] == "Test title"
+    assert result_data["description"] == "Test description"
+
+
+def test_update_task(client: TestClient):
+    response = client.post("/tasks/", json={
+        "title": "Test title",
+        "description": "Test description"
+    })
+    data = response.json()
+    task_id = data["id"]
+    update_response = client.patch(f"/tasks/{task_id}", json={
+        "title": "New test title"
+    })
+    update_data = update_response.json()
+    assert update_response.status_code == 200
+    assert update_data["title"] == "New test title"
+    assert update_data["description"] == "Test description"
+
+
+def test_delete_task(client: TestClient):
+    response = client.post("/tasks/", json={
+        "title": "Test title",
+        "description": "Test description"
+    })
+    data = response.json()
+    task_id = data["id"]
+    delete_response = client.delete(f"/tasks/{task_id}")
+    get_response = client.get(f"/tasks/{task_id}")
+    assert delete_response.status_code == 204
+    assert get_response.status_code == 404
+    
