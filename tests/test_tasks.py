730b0d920418c33317ffa02a8461ef0c4a57a107
Damian Marchewka <enki6@o2.pl>
2026-05-01 21:37:33 +0200

    Added error tests for CRUD tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,6 +2,8 @@ import pytest
 from fastapi.testclient import TestClient
 
 
+# === SUCCESS TESTS ===
+
 def test_create_task(client: TestClient):
     response = client.post("/tasks/", json={
         "title": "Test title",
@@ -55,4 +57,39 @@ def test_delete_task(client: TestClient):
     get_response = client.get(f"/tasks/{task_id}")
     assert delete_response.status_code == 204
     assert get_response.status_code == 404
-    
+
+
+# === ERROR TESTS ===
+
+def test_get_task_not_found(client: TestClient):
+    response = client.get("/tasks/9999")
+    data = response.json()
+    assert response.status_code == 404
+    assert data["detail"] == "Task not found"
+
+
+def test_update_task_not_found(client: TestClient):
+    response = client.patch("/tasks/9999", json={
+        "title": "New title"
+    })
+    data = response.json()
+    assert response.status_code == 404
+    assert data["detail"] == "Task not found"
+
+
+def test_update_task_empty_body(client: TestClient):
+    response = client.post("/tasks/", json={
+        "title": "Test title",
+        "description": "Test description"
+    })
+    data = response.json()
+    task_id = data["id"]
+    response_patch = client.patch(f"/tasks/{task_id}", json={})
+    data_patch = response_patch.json()
+    assert response_patch.status_code == 400
+    assert data_patch["detail"] == "Empty data"
+
+
+def test_delete_task_not_found(client: TestClient):
+    response = client.delete("/tasks/9999")
+    assert response.status_code == 404
