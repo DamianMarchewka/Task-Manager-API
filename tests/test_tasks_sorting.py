044468ed54d27f2579d95620cceb582629c60976
Damian Marchewka <enki6@o2.pl>
2026-09-14 19:57:07 +0200

    Add sorting test (sorting by title asc)

--- tests/test_tasks_sorting.py
+++ tests/test_tasks_sorting.py
@@ -1,2 +1,24 @@
 import pytest
 from fastapi.testclient import TestClient
+
+def test_task_sorting_title_asc(client: TestClient):
+    client.post("/tasks/", json={
+        "title": "C task",
+        "description": "test description"
+    })
+    client.post("/tasks/", json={
+        "title": "A task",
+        "description": "test description"
+    })
+    client.post("/tasks/", json={
+        "title": "B task",
+        "description": "test description"
+    })
+
+    response = client.get("/tasks/?sort_by=title&order=asc")
+    data = response.json()
+    titles = [task["title"] for task in data]
+
+    assert response.status_code == 200
+    assert len(data) == 3
+    assert titles == ["A task", "B task", "C task"]
