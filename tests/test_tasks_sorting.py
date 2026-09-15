446102c53d4d8236faf2c84168e6bf551009bff3
Damian Marchewka <enki6@o2.pl>
2026-09-15 19:04:34 +0200

    Add sorting test (sorting by title desc)

--- tests/test_tasks_sorting.py
+++ tests/test_tasks_sorting.py
@@ -22,3 +22,27 @@ def test_task_sorting_title_asc(client: TestClient):
     assert response.status_code == 200
     assert len(data) == 3
     assert titles == ["A task", "B task", "C task"]
+
+
+def test_task_sorting_title_desc(client: TestClient):
+    client.post("/tasks/", json={
+        "title": "C task",
+        "description": "test description"
+    })
+    client.post("/tasks/", json={
+        "title": "A task",
+        "description": "test description"
+    })
+    client.post("/tasks/", json={
+        "title": "B task",
+        "description": "test description"
+    })
+
+    response = client.get("/tasks/?sort_by=title&order=desc")
+    data = response.json()
+    titles = [task["title"] for task in data]
+    print(titles)
+
+    assert response.status_code == 200
+    assert len(data) == 3
+    assert titles == ["C task", "B task", "A task"]
